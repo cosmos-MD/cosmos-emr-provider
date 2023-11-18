4d038e79f4d10476e1e0edacc9d739ac6a69d00b
Michael Yingbull <michael.yingbull@cosmosmd.ca>
2023-11-18 16:28:48 -0500

    Adding directory in components to sort demographic related ones.  Added HumanNameInput.tsx component from medplum-react library for future motification.

---
+++ src/components/demographics/HumanNameInput.tsx
@@ -0,0 +1,93 @@
+import { Group, NativeSelect, TextInput } from '@mantine/core';
+import { HumanName } from '@medplum/fhirtypes';
+import React, { useRef, useState } from 'react';
+
+export interface HumanNameInputProps {
+  name: string;
+  defaultValue?: HumanName;
+  onChange?: (value: HumanName) => void;
+}
+
+export function HumanNameInput(props: HumanNameInputProps): JSX.Element {
+  const [value, setValue] = useState<HumanName | undefined>(props.defaultValue);
+
+  const valueRef = useRef<HumanName>();
+  valueRef.current = value;
+
+  function setValueWrapper(newValue: HumanName): void {
+    setValue(newValue);
+    if (props.onChange) {
+      props.onChange(newValue);
+    }
+  }
+
+  function setUse(use: 'temp' | 'old' | 'usual' | 'official' | 'nickname' | 'anonymous' | 'maiden' | undefined): void {
+    setValueWrapper({ ...valueRef.current, use: use || undefined });
+  }
+
+  function setPrefix(prefix: string): void {
+    setValueWrapper({
+      ...valueRef.current,
+      prefix: prefix ? prefix.split(' ') : undefined,
+    });
+  }
+
+  function setGiven(given: string): void {
+    setValueWrapper({
+      ...valueRef.current,
+      given: given ? given.split(' ') : undefined,
+    });
+  }
+
+  function setFamily(family: string): void {
+    setValueWrapper({
+      ...valueRef.current,
+      family: family || undefined,
+    });
+  }
+
+  function setSuffix(suffix: string): void {
+    setValueWrapper({
+      ...valueRef.current,
+      suffix: suffix ? suffix.split(' ') : undefined,
+    });
+  }
+
+  return (
+    <Group spacing="xs" grow noWrap>
+      <NativeSelect
+        defaultValue={value?.use}
+        name={props.name + '-use'}
+        data-testid="use"
+        onChange={(e) =>
+          setUse(e.currentTarget.value as 'temp' | 'old' | 'usual' | 'official' | 'nickname' | 'anonymous' | 'maiden')
+        }
+        data={['', 'temp', 'old', 'usual', 'official', 'nickname', 'anonymous', 'maiden']}
+      />
+      <TextInput
+        placeholder="Prefix"
+        name={props.name + '-prefix'}
+        defaultValue={value?.prefix?.join(' ')}
+        onChange={(e) => setPrefix(e.currentTarget.value)}
+      />
+      <TextInput
+        placeholder="Given"
+        name={props.name + '-given'}
+        defaultValue={value?.given?.join(' ')}
+        onChange={(e) => setGiven(e.currentTarget.value)}
+      />
+      <TextInput
+        name={props.name + '-family'}
+        placeholder="Family"
+        defaultValue={value?.family}
+        onChange={(e) => setFamily(e.currentTarget.value)}
+      />
+      <TextInput
+        placeholder="Suffix"
+        name={props.name + '-suffix'}
+        defaultValue={value?.suffix?.join(' ')}
+        onChange={(e) => setSuffix(e.currentTarget.value)}
+      />
+    </Group>
+  );
+}
