4eb58dd0d07c76ffd9cfd166d0ebb3bb9cfd40ab
yingbull <8680161+yingbull@users.noreply.github.com>
2023-11-23 12:43:48 -0500

    Add Basic Styling and Structure to Demographics.tsx - background and text color now change per Mantine theme

--- src/components/Demographics.tsx
+++ src/components/Demographics.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { Box, Text, Flex, useMantineTheme } from '@mantine/core';
 
 export function Demographics(): JSX.Element {
-  // Get the current color scheme from Mantine theme
   const { colorScheme } = useMantineTheme();
 
-  // Determine the background color based on the color scheme
+  // Adjusting background and text color based on the theme
   const backgroundColor = colorScheme === 'dark' ? '#333' : '#f8f8f8';
-  
+  const textColor = colorScheme === 'dark' ? '#fff' : '#000';
+
   return (
     <Flex direction="column" align="center" style={{ padding: '2px' }}>
       <Box style={{ width: '80%', textAlign: 'left', background: backgroundColor }}>
-        <Text size="xl" weight={700}>
+        <Text
+          size="xl"
+          weight={700}
+          style={{ color: textColor, padding: '4px 8px', borderRadius: '4px' }}
+        >
           Demographics
         </Text>
+        {/* Placeholder for future component */}
       </Box>
     </Flex>
   );
