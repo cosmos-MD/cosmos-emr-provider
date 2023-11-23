4ab95fee8e0067781b6ff3f677c18eab2cf2a4c5
yingbull <8680161+yingbull@users.noreply.github.com>
2023-11-23 12:33:51 -0500

    Demographics.tsx basic structure update This commit is the first update to Demographics.  It adds Mantine and sets up a Flex and Box component with the title of Demographics contained within (where the Demographics display will go). Updated to have this component get dynamic colour scheme per Mantine. This allows for further updates to develop this component.

--- src/components/Demographics.tsx
+++ src/components/Demographics.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Container, Text } from '@mantine/core';
-
+import { Box, Text, Flex, useMantineTheme } from '@mantine/core';
 
 export function Demographics(): JSX.Element {
-  return (
-    <Container size="xs">
-      <Text align="center" size="xl">
-        Demographics
-      </Text>
-    </Container>
-  );
-};
+  // Get the current color scheme from Mantine theme
+  const { colorScheme } = useMantineTheme();
 
+  // Determine the background color based on the color scheme
+  const backgroundColor = colorScheme === 'dark' ? '#333' : '#f8f8f8';
+  
+  return (
+    <Flex direction="column" align="center" style={{ padding: '2px' }}>
+      <Box style={{ width: '80%', textAlign: 'left', background: backgroundColor }}>
+        <Text size="xl" weight={700}>
+          Demographics
+        </Text>
+      </Box>
+    </Flex>
+  );
+}
